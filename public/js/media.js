03e47c84d3244b209a3d920dabf67675429a5b71
wh <liuyuntie@gmail.com>
2018-12-26 20:23:27 +0800

    add web

---
+++ public/js/media.js
@@ -0,0 +1,98 @@
+function ajaxGetSongData(url){
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.responseType = 'arraybuffer';
+        EventUtil.addEvent(xhr, 'load', () => {
+            resolve(xhr.response);
+        });
+        xhr.open('GET', url, true);
+        xhr.send(null);
+    });
+}
+
+class Media{
+    _emtry(){
+        this.pause();
+        this.ctx = null;
+        this.source = null;
+        this.analyser = null;
+    }
+    decode(buffer = Blob){
+        return new Promise((resolve, reject) => {
+            if(this.ctx !== undefined){
+                this._emtry();
+            }
+            this.ctx = new (AudioContext || webkitAudioContext)();
+            this.source = this.ctx.createBufferSource();
+            this.ctx.decodeAudioData(buffer).
+                then((data) => {
+                    this.source.buffer = data;
+                    this.analyser = this.ctx.createAnalyser();
+                    this.source.connect(this.analyser);
+                    this.analyser.connect(this.ctx.destination);
+                    this.source.start(0);
+                    resolve();
+                });
+        });
+
+    }
+    play(){
+        if(this.analyser !== undefined){
+            this.analyser.connect(this.ctx.destination);
+            return true;
+        }
+    }
+    pause(){
+        if(this.ctx !== undefined){
+            this.analyser.disconnect(this.ctx.destination);
+            return false;
+        }
+    }
+}
+function readLocalFile(file, rState = 'o'){
+    return new Promise((resolve, reject) => {
+        let r = new FileReader();
+        r.addEventListener('load', (e) => {
+            resolve(e.target.result);
+        });
+        switch(rState){
+            case 'o':
+                r.readAsArrayBuffer(file);
+            break;
+            case 'u':
+                r.readAsDataURL(file);
+            break;
+            case 't':
+                r.readAsText(file);
+            break;
+            default:
+                reject(`rstate is not ${rState}`);
+        }
+        
+    });
+}
+class Lyric{
+    parseLrc(text = String){
+        let lyrics = text.split(/\n/g),
+            lyricMap = {};
+        lyrics.forEach((lyric) => {
+            let [m, s] = [
+                parseInt(lyric.match(/.\:/i)),
+                parseInt(lyric.match(/.{2}\..{2}/i))
+            ];
+            let text = lyric.match(/[^\[\]]+$/gi),
+                time = m * 60 + s;
+            lyricMap[time] = text;
+        });
+        return lyricMap;
+    }
+    render(lyricMap = Object, showNodeName = 'li'){
+        this.nodeMap = {};
+        for(let time in lyricMap){
+            let n = document.createElement(showNodeName);
+            n.innerText = lyricMap[time];
+            this.nodeMap[time] = n;
+        }
+        return this.nodeMap;
+    }
+}
